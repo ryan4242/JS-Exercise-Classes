d3fdbd9ef7c8af4a7c92e6f8440d2645beb1bf41
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-04-29 18:47:55 -0700

    Lambdasian class passed

--- index.js
+++ index.js
@@ -116,7 +116,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(obj) {
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  };
 
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  };
 }
 
 /*
