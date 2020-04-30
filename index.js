59cfbcc62cba825b788cef9b3a24ac343529cfa0
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-04-29 19:05:26 -0700

    Student class passed

--- index.js
+++ index.js
@@ -173,8 +173,25 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(obj) {
+    super(obj);
+    this.previousBackground = obj.previousBackground;
+    this.className = obj.className;
+    this.favSubjects = obj.favSubjects;
+  };
 
+  listSubjects() {
+    return `Loving ${this.favSubjects.toString()}!`;
+  };
+
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  };
+
+  sprintChallenge(subject) {
+    return `${this.name} has begun challenge on ${subject}`;
+  };
 }
 
 /*
